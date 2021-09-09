23935479c4088cf1e4bb35d0123794a913f54605
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2021-09-09 10:48:42 +0100

    Add tooling MVC

---
+++ database/migrations/2021_09_09_091742_create_tools_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateToolsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tools', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('link');
+            $table->string('version');
+            $table->integer('license_id');
+            $table->integer('contact_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tools');
+    }
+}
