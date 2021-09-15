0973dea952a20c07e4e01bc14e63fc6bb543b4b1
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2021-09-15 22:52:39 +0100

    Improve ToolEvent test

--- tests/Unit/ToolEventsTest.php
+++ tests/Unit/ToolEventsTest.php
@@ -6,12 +6,11 @@ use App\Models\Event;
 use App\Models\Tool;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ToolEventsTest extends TestCase
 {
-    use RefreshDatabase, WithFaker;
+    use RefreshDatabase;
 
     /** @test */
     public function tools_can_record_a_review_event()
@@ -22,7 +21,7 @@ class ToolEventsTest extends TestCase
         $tool = Tool::factory()->create();
         $user = User::factory()->create();
 
-        $detail = 'This is a review of a tool submitted to the TPC, by a user.';
+        $detail = 'This is a viability review of a tool submitted to the TPC, by an authenticated user.';
 
         $tool->review($detail, $user);
 
@@ -40,10 +39,12 @@ class ToolEventsTest extends TestCase
          **/
         $tool = Tool::factory()->create();
 
+        $tool->status('in review');
+        $tool->status('rejected');
         $tool->status('approved');
 
-        $this->assertCount(1, Event::all());
-        $this->assertEquals('approved', Event::first()->detail);
+        $this->assertCount(3, Event::all());
+        $this->assertEquals('in review', Event::first()->detail);
         $this->assertEquals($tool->id, Event::first()->tool_id);
     }
 }
