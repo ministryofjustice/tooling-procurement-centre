60ba68e53edc32073d94193a17e5cf0b46ca4442
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2021-09-16 15:47:45 +0100

    Add favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="{{ asset('judiciary-icon.png') }}" type="image/x-icon">
 
     <title>{{ config('app.name', 'Tool Procurement Centre | MoJ D&T') }}</title>
 
